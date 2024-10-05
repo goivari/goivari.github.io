576c2a255fed74a16972ab8a972b963c9dfe897d
viktor <biolefi@bk.ru>
2024-10-05 12:18:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active');
     showcase.classList.toggle('active');
 })
+
+const modal = document.querySelector('dialog')
+const modalBox = document.getElementById('modal-box')
+const showModalBtn = document.getElementById('show-modal-btn')
+const closeModalBtn = document.getElementById('close-modal-btn')
+
+let isModalOpen = false
+
+showModalBtn.addEventListener('click', (e) => {
+  modal.showModal()
+  isModalOpen = true
+  e.stopPropagation()
+})
+
+closeModalBtn.addEventListener('click', () => {
+  modal.close()
+  isModalOpen = false
+})
+
+document.addEventListener('click', (e) => {
+  if (isModalOpen && !modalBox.contains(e.target)) {
+    modal.close()
+  }
+})
